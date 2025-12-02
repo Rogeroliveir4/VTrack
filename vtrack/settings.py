fbfc384971a1c7aa82812a6ae7c4b728a208bd7c
Roger Oliveira <41408566+Rogeroliveir4@users.noreply.github.com>
2025-12-01 22:39:10 -0300

    Fix ALLOWED_HOSTS for Railway

--- vtrack/settings.py
+++ vtrack/settings.py
@@ -15,12 +15,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '.railway.app',
-    'vtrack-production.up.railway.app',
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['*']
+
 
 
 
